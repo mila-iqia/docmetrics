5b2a7bb115cad878d001b907883f924264a5415f
Fabrice Normandin <normandf@mila.quebec>
2025-11-25 13:15:17 -0500

    Use structured output to constrain answers
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import argparse
 from pathlib import Path
+import pydantic
 from pydantic.dataclasses import dataclass
 import yaml
 from google import genai
 from google.genai import types
-import textwrap
-import httpx
+import rich.logging
 import logging
 
 logger = logging.getLogger(__name__)
@@ -35,6 +35,10 @@ doc_url = "https://docs.mila.quebec"
 # doc_data = httpx.get(doc_url).content
 
 
+class Response(pydantic.BaseModel):
+    answer: int = pydantic.Field(description="The selected answer (integer).", ge=1)
+
+
 def evaluate_llm(
     questions: list[Question], context: str | None = None, llm: str = "gemini-2.5-flash"
 ) -> int:
@@ -59,20 +63,26 @@ def evaluate_llm(
             + answers_block
             + f"Only provide the answer as a single integer with no explanation: ({possible_answers}):\n"
         )
-        response = client.models.generate_content(model=llm, contents=contents)
+        logger.debug(f"Prompt sent to LLM: {contents}")
+        response = client.models.generate_content(
+            model=llm,
+            contents=contents,
+            config={
+                "response_mime_type": "application/json",
+                "response_json_schema": Response.model_json_schema(),
+            },
+        )
+        logger.info(f"Agent response: {response.text}")
         assert response.text
-
-        if (
-            not response.text
-            or not (agent_answer := response.text.splitlines()[-1]).isnumeric()
-        ):
-            logger.error(f"Invalid guess: {response.text}")
-            invalid_answers += 1
-            continue
-
-        agent_answer = int(agent_answer)
+        response = Response.model_validate_json(response.text)
+        agent_answer = response.answer
+        # if (
+        #     response
+        # ):
+        #     logger.error(f"Invalid guess: {response.text}")
+        #     invalid_answers += 1
+        #     continue
         correct_answer = question.answers.index(question.correct_answer) + 1
-
         logger.debug(f"LLM answer: {agent_answer}, correct answer: {correct_answer}")
         if agent_answer == correct_answer:
             correct_answers += 1
@@ -86,6 +96,22 @@ def main():
     args = parser.parse_args()
     questions_path: Path = args.questions
     verbose: int = args.verbose
+
+    logging.basicConfig(
+        level=logging.DEBUG
+        if verbose >= 3
+        else logging.INFO
+        if verbose == 2
+        else logging.WARNING,
+        handlers=[rich.logging.RichHandler()],
+    )
+    logger.setLevel(
+        logging.DEBUG
+        if verbose >= 2
+        else logging.INFO
+        if verbose == 1
+        else logging.WARNING
+    )
 
     questions = load_questions(questions_path=questions_path)
     score_with_no_context = evaluate_llm(questions, context=None)
