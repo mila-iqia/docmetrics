ed960cf563e56b65150dcdb5183f15db91c84ba3
Fabrice Normandin <normandf@mila.quebec>
2025-11-25 10:57:07 -0500

    Initial commit
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

---
+++ README.md
@@ -0,0 +1,11 @@
+# DocMetrics
+
+DocMetrics is a tool to analyze and measure the complexity and usefulness of documentation.
+
+It does this using a set of multiple choice questions, that you wish your users to be able to answer after reading the documentation.
+
+An LLM is then asked to answer these questions, with and without the documentation as part of its context. The difference in score is then used as a measure of the usefulness of the documentation.
+
+The list of questions need to be provided to the tool by passing a `--questions` option pointing to a yaml file. See the `sample_questions.yaml` file for an example.
+
+The choice of LLM can be configured, but defaults to Google's Gemini model. The set of tools available to the agent can also be configured.
---
+++ main.py
@@ -0,0 +1,96 @@
+import argparse
+from pathlib import Path
+from pydantic.dataclasses import dataclass
+import yaml
+from google import genai
+from google.genai import types
+import textwrap
+import httpx
+import logging
+
+logger = logging.getLogger(__name__)
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client()
+
+
+@dataclass
+class Question:
+    question: str
+    answers: list[str]
+    correct_answer: str
+
+    def __postinit__(self):
+        assert self.correct_answer in self.answers, (
+            "correct answer isn't in the options!"
+        )
+
+
+def load_questions(questions_path: Path) -> list[Question]:
+    return [Question(**q) for q in yaml.safe_load(questions_path.read_text())]
+
+
+doc_url = "https://docs.mila.quebec"
+
+# Retrieve and encode the PDF byte
+# doc_data = httpx.get(doc_url).content
+
+
+def evaluate_llm(
+    questions: list[Question], context: str | None = None, llm: str = "gemini-2.5-flash"
+) -> int:
+    """Evaluates a given LLM with the given questions and context. Returns the number of correct answers."""
+    correct_answers = 0
+    invalid_answers = 0
+    for question in questions:
+        answers_block = (
+            "\n"
+            + "\n".join(
+                f"{i + 1}. {answer}" for i, answer in enumerate(question.answers)
+            )
+            + "\n"
+        )
+        possible_answers = (
+            ", ".join(str(i) for i in range(len(question.answers) - 1))
+            + f" or {len(question.answers)}"
+        )
+        contents = (
+            "Select the correct answer to the following question:\n"
+            + f"- {question.question}\n"
+            + answers_block
+            + f"Only provide the answer as a single integer with no explanation: ({possible_answers}):\n"
+        )
+        response = client.models.generate_content(model=llm, contents=contents)
+        assert response.text
+
+        if (
+            not response.text
+            or not (agent_answer := response.text.splitlines()[-1]).isnumeric()
+        ):
+            logger.error(f"Invalid guess: {response.text}")
+            invalid_answers += 1
+            continue
+
+        agent_answer = int(agent_answer)
+        correct_answer = question.answers.index(question.correct_answer) + 1
+
+        logger.debug(f"LLM answer: {agent_answer}, correct answer: {correct_answer}")
+        if agent_answer == correct_answer:
+            correct_answers += 1
+    return correct_answers
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--questions", type=Path, required=True)
+    parser.add_argument("-v", "--verbose", action="count")
+    args = parser.parse_args()
+    questions_path: Path = args.questions
+    verbose: int = args.verbose
+
+    questions = load_questions(questions_path=questions_path)
+    score_with_no_context = evaluate_llm(questions, context=None)
+    print(score_with_no_context)
+
+
+if __name__ == "__main__":
+    main()
